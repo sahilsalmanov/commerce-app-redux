623f2a6a5f0ac78dff6934ab03fc83c4af44e97e
Sahil Salmanov <101056019+sahilsalmanov@users.noreply.github.com>
2023-05-27 15:09:49 +0400

    Add files via upload

---
+++ src/store/reducers/reducers.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux';
+
+const cartItemsReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, { ...action.payload, quantity: 1 }];
+    case 'REMOVE_FROM_CART':
+      return state.filter((item) => item.id !== action.payload);
+    case 'INCREASE_QUANTITY':
+      return state.map((item) =>
+        item.id === action.payload ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    case 'DECREASE_QUANTITY':
+      return state.map((item) =>
+        item.id === action.payload && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      );
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cartItems: cartItemsReducer
+});
+
+export default rootReducer;
